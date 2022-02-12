c259198090b7508177100cb6b7c67af21e1d4d2f
shaiq-dev <shaiqkar@gmail.com>
2022-02-13 02:19:29 +0530

    added resume retreiver from github api

---
+++ src/functions.py
@@ -0,0 +1,72 @@
+import os
+from os.path import join as _j
+import pathlib
+from functools import lru_cache
+from zipfile import ZipFile
+import requests
+
+ROOT = pathlib.Path(__file__).parent.parent
+
+
+class ResumeExtractor:
+
+    def __init__(self, content, filename, id, root=ROOT, artifact_file="resume.pdf"):
+        self.content = content
+        self.filename = filename
+        self.artifact_file = artifact_file
+
+        self._store_path = _j(root, 'store', str(id))
+
+    def _write_and_extract_zip(self):
+        if not os.path.exists(self._store_path):
+            os.mkdir(self._store_path)
+            file = _j(self._store_path, f'{self.filename}.zip')
+
+            # Write the zip file to disk
+            with open(file, 'wb') as f:
+                f.write(self.content)
+
+            # Extract the zip file
+            with ZipFile(file, 'r') as zip:
+                zip.extractall(self._store_path)
+
+            # Rename the extracted file
+            os.rename(_j(self._store_path, self.artifact_file),
+                      _j(self._store_path, f'{self.filename}.pdf'))
+
+    def _get_pdf_version(self):
+        with open(_j(self._store_path, 'version.txt'), 'r') as f:
+            return f.read()[:-2]
+
+    def get_pdf(self):
+        self._write_and_extract_zip()
+        pdf_path = _j(self._store_path, f'{self.filename}.pdf')
+        return (f'{self.filename}.pdf', pdf_path, self._get_pdf_version())
+
+
+@lru_cache(maxsize=None)
+def get_resume_from_artifact():
+    """
+    Get a list of all artifacts for a given repo
+    """
+    owner = os.environ.get("REPO_OWNER")
+    repo = os.environ.get("REPO_NAME")
+    token = os.environ.get("GITHUB_PAT")
+
+    headers = {
+        'Authorization': f'token {token}',
+        'Accept': 'application/vnd.github.v3+json'
+    }
+    url = f"https://api.github.com/repos/{owner}/{repo}/actions/artifacts"
+    _r = requests.get(url, headers=headers)
+
+    # Get the latest artifact
+    artifact = _r.json()['artifacts'][0]
+    response = requests.get(
+        artifact['archive_download_url'], headers=headers, stream=True)
+
+    # Write the artifact to disk and extract the pdf
+    pdf_data = ResumeExtractor(response.content,
+                               filename="ResumeShaiqKar", id=artifact['id']).get_pdf()
+
+    return pdf_data
---
+++ src/server.py
@@ -0,0 +1,38 @@
+from functools import lru_cache
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.responses import FileResponse, JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+from functions import get_resume_from_artifact
+
+# Load all the environment variables from .env
+load_dotenv()
+
+app = FastAPI()
+# To allow cross origin request to the app
+# we need to add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/resume", response_class=FileResponse)
+async def get_resume():
+    pdf_name, pdf_path, *_ = get_resume_from_artifact()
+    return FileResponse(pdf_path, media_type="application/pdf", filename=pdf_name)
+
+
+@app.get("/resume/version")
+async def get_resume_version():
+    *_, _v = get_resume_from_artifact()
+    return JSONResponse(content={"version": _v})
